b6f361b31f1bc4c26ee384131e9cfc981f9620ac
Martin Eden <martin.s.eden@pm.me>
2018-09-13 15:19:14 +0100

    Don't wipe volumes

--- aws_metrics/create_configuration_volume.sh
+++ aws_metrics/create_configuration_volume.sh
@@ -2,7 +2,6 @@
 set -ex
 here=$(dirname $0)
 
-docker volume rm aws_metrics_config || echo "No volume to remove"
 $here/configure/configure.py > aws_configuration
 tmp=$(docker run -dt --rm -v aws_metrics_config:/data alpine)
 docker cp aws_configuration $tmp:/data/credentials
